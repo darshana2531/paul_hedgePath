49f478c912dcd6919c40e3faa5c53bbd21cd94e2
darshana pawade <darshana.pawade@smartdatain.net>
2022-12-10 11:57:03 +0530

    create modules

---
+++ hedgepath/src/app/module/contactus/contactus.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactusComponent } from './contactus.component';
+
+
+
+@NgModule({
+  declarations: [
+    ContactusComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ContactusModule { }
