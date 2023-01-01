7a99295a4a87f1373c1d68ecfe963de371776dc9
nkomalcg@gmail.com <nkomalcg@gmail.com>
2023-01-01 17:31:55 +0530

    Routing Samples added

--- hedgepath/src/app/app-routing.module.ts
+++ hedgepath/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('src/app/module/home/home.module').then(m => m.HomeModule)
+
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('src/app/module/about/about.module').then(m => m.AboutModule)
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
